12a66520dcbfb0646cd03f34446d6a1d3efa9b16
Daniele Martinoli <dmartino@redhat.com>
2025-06-26 09:24:17 +0200

    updated GH org to RHEcosystemAppEng
    Signed-off-by: Daniele Martinoli <dmartino@redhat.com>

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -27,7 +27,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/dmartinol/mcp-registry-operator/test/utils"
+	"github.com/RHEcosystemAppEng/mcp-registry-operator/test/utils"
 )
 
 // namespace where the project is deployed in
